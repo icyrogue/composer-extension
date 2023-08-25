ffdb181ff66d02da3697ef2da7c3efe964999812
Dmtriy Tumanov <89037640+icyrogue@users.noreply.github.com>
2023-08-25 18:09:12 +0200

    hide top and bottom; stop on the last frame

--- content.js
+++ content.js
@@ -3,6 +3,28 @@
   if (!onYT) {
       return;
   }
+
+  const player = document.querySelector("video")
+  if (!player) {
+      return;
+  }
+
+ var intv = setInterval(updatePlayerInfo, 250);
+ var prev = player.currentTime
+
+  function updatePlayerInfo() {
+    duration = player.duration.toPrecision(3)
+    current = player.currentTime.toPrecision(3)
+    frame = (current - prev).toPrecision(3)
+    console.log(frame)
+
+    if (current == duration && duration > 0 && (duration - frame*2) <= current && current <= (duration + frame*2)) {
+        player.pause();
+        clearInterval(intv);
+    }
+    prev = current 
+  } 
+
   hide()
 
   chrome.runtime.onMessage.addListener(
@@ -11,7 +33,6 @@
         hide()
         sendResponse({hidden: true});
       } else {
-        console.log(request.command)
         show()
         sendResponse({hidden: false});
       }
@@ -24,6 +45,8 @@ function hide() {
   document.getElementsByClassName('ytp-chrome-controls')[0].style.visibility = 'hidden';
   document.getElementsByClassName('ytp-gradient-top')[0].style.visibility = 'hidden';
   document.getElementsByClassName('ytp-gradient-bottom')[0].style.visibility = 'hidden';
+  document.getElementsByClassName('ytp-chrome-top')[0].style.visibility = 'hidden';
+  document.getElementsByClassName('ytp-chrome-bottom')[0].style.visibility = 'hidden';
   document.getElementsByClassName('ytp-progress-bar')[0].style.visibility = 'hidden';
   document.getElementsByClassName('ytp-large-play-button')[0].style.visibility = 'hidden';
   document.getElementsByClassName('ytp-progress-bar-container')[0].style.visibility = 'hidden';
@@ -33,6 +56,8 @@ function hide() {
 }
 
 function show() {
+  document.getElementsByClassName('ytp-chrome-top')[0].style.visibility = 'visible';
+  document.getElementsByClassName('ytp-chrome-bottom')[0].style.visibility = 'visible';
   document.getElementsByClassName('ytp-progress-bar')[0].style.visibility = 'visible';
   document.getElementsByClassName('ytp-chrome-controls')[0].style.visibility = 'visible';
   document.getElementsByClassName('ytp-gradient-top')[0].style.visibility = 'visible';
